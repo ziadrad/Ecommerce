649112ba0277939c15d23e5d2d96d70ded5c4c89
ziadrad <ziadrady60@gmail.com>
2024-08-16 17:22:45 +0300

    13 commit

--- src/app/layout/additions/footer/footer.component.ts
+++ src/app/layout/additions/footer/footer.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
